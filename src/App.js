657502f0417621af1e81fcf791037b43b853f5e9
Sourabh76 <sourabhingale76@gmail.com>
2024-05-03 15:52:27 +0530

    Added job description and UI changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { useState, useEffect } from "react";
 import getSymbolFromCurrency from "currency-symbol-map";
+import Timer from "./assets/images/timer.svg";
+import Company1 from "./assets/images/company1.svg";
+import Company2 from "./assets/images/company2.svg";
+import Company3 from "./assets/images/company3.svg";
+import Close from "./assets/images/close.svg";
 import "./App.css";
 
 function App() {
   const [jobListings, setJobListings] = useState([]);
+  const [viewMore, setViewMore] = useState(false);
   const [filteredJobListings, setFilteredJobListings] = useState([]);
   const [filters, setFilters] = useState({
     minExperience: "",
@@ -16,6 +22,19 @@ function App() {
   });
   const [page, setPage] = useState(1);
   const perPage = 10;
+
+  // This function is used to get random company image
+  const getCompanyImg = () => {
+    const val = Math.floor(Math.random() * 3);
+    switch (val) {
+      case 1:
+        return Company1;
+      case 2:
+        return Company2;
+      default:
+        return Company3;
+    }
+  };
 
   useEffect(() => {
     fetchJobListings(page);
@@ -173,38 +192,87 @@ function App() {
           />
         </label>
       </div> */}
-      <div className="job-listings">
+      <div
+        className="job-listings"
+        id="job-listings"
+        style={{
+          opacity: viewMore ? 0.3 : 1.0,
+        }}
+      >
         {filteredJobListings.map((listing, index) => (
           <div className="job">
             <div className="job-card" key={index}>
-              <p>
-                <strong>Job Title:</strong> {listing.jobRole}
+              <p className="post-time">
+                <img src={Timer} alt="timer" /> &nbsp; Posted 10 days ago
               </p>
-              <p>
-                <strong>Company Name:</strong> {listing.jdUid.slice(-5)}
-              </p>
-              <p>
-                <strong>Location:</strong> {listing.location}
-              </p>
-              <p>
-                <strong>Estimated Salary:</strong>{" "}
+              <div className="job-name">
+                <img src={getCompanyImg()} alt="logo" />
+                <div className="job-info">
+                  <div className="info-container">
+                    <h3>{listing.jdUid.slice(-5)}</h3>
+                    <h2>{listing.jobRole}</h2>
+                  </div>
+                  <p>{listing.location}</p>
+                </div>
+              </div>
+              <p className="job-salary">
+                Estimated Salary:{" "}
                 {getSymbolFromCurrency(listing.salaryCurrencyCode)}
                 {`${listing.minJdSalary ? listing.minJdSalary : 0} - ${
                   listing.maxJdSalary
                 }  ✅`}
               </p>
-              <p>
-                <strong>Job Description:</strong>{" "}
-                {listing.jobDetailsFromCompany}
-              </p>
-              <p>
-                <strong>Experience Required:</strong> {listing.minExp}
-              </p>
-              <button>Apply Button/Link</button>
+              <div className="job-description">
+                <p className="about-company">About Company:</p>
+                <div className="about-us">
+                  <p>
+                    <strong>About us</strong>
+                  </p>
+                  <p>{listing.jobDetailsFromCompany}</p>
+                </div>
+              </div>
+              <div className="view-job" onClick={() => setViewMore(true)}>
+                View Job
+              </div>
+              <div className="job-experience">
+                <h3>Minimum Experience</h3>
+                <div>{listing.minExp ? listing.minExp : 0} years</div>
+              </div>
+              <div className="apply-button">
+                <button
+                  className="easy-apply"
+                  onClick={() => alert("Applied!!")}
+                >
+                  ⚡ Easy Apply
+                </button>
+                <button
+                  className="referral"
+                  onClick={() => alert("Referred!!")}
+                >
+                  <img
+                    src="https://media.licdn.com/dms/image/C5103AQHRgnM-GkuZmA/profile-displayphoto-shrink_100_100/0/1549103266658?e=1715817600&v=beta&t=qxtZ_x6KLpbOr-4cDJBp42cEoBKAVabypWSf-7c9Cic"
+                    alt="referral"
+                  />
+                  <img
+                    src="https://media.licdn.com/dms/image/C5103AQHRgnM-GkuZmA/profile-displayphoto-shrink_100_100/0/1549103266658?e=1715817600&v=beta&t=qxtZ_x6KLpbOr-4cDJBp42cEoBKAVabypWSf-7c9Cic"
+                    alt="referral"
+                  />
+                  <span>Unlock referral asks</span>
+                </button>
+              </div>
             </div>
           </div>
         ))}
       </div>
+      {viewMore && (
+        <div>
+          <div className="full-description">
+            <img src={Close} alt="close" onClick={() => setViewMore(false)} />
+            <h2>Job description</h2>
+            <div>{jobListings[0].jobDetailsFromCompany}</div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
